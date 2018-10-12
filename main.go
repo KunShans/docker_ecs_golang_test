c2fb5eed517ef36e4824886ea6740bd9aa48bb2f
Ks <cks@luckyzune.com>
2018-10-12 13:57:40 +0800

    iris

--- main.go
+++ main.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"github.com/labstack/gommon/log"
+	"github.com/kataras/iris"
+	"github.com/kataras/iris/context"
 )
 
 func main() {
 	fmt.Println("Hello ks docker")
-	http.HandleFunc("/ping", home)
-	http.ListenAndServe(":80", nil)
+	//http.HandleFunc("/ping", home)
+	//http.ListenAndServe(":80", nil)
+
+	app := iris.New()
+
+	app.Get("/ping", func(ctx context.Context) {
+		log.Infof("ping")
+		ctx.WriteString("pong pong")
+	})
+	app.Run(iris.Addr(":8080"), iris.WithCharset("UTF-8"))
+
+	fmt.Println("Hello ks docker 2")
+
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("welcome to ks travel")
-	fmt.Fprintf(w, "welcome to ks travel")
-}
+//func home(w http.ResponseWriter, r *http.Request) {
+//	fmt.Println("welcome to ks travel")
+//	fmt.Fprintf(w, "welcome to ks travel")
+//}
