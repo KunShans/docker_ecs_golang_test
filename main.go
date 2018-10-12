72b6e0db38415d7c9f78a23de400bd6ec96e4b3c
Ks <cks@luckyzune.com>
2018-10-12 14:02:09 +0800

    8080

--- main.go
+++ main.go
@@ -2,34 +2,32 @@ package main
 
 import (
 	"fmt"
-	"github.com/labstack/gommon/log"
-	"github.com/kataras/iris"
-	"github.com/kataras/iris/context"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Hello ks docker")
-	//http.HandleFunc("/ping", home)
-	//http.ListenAndServe(":80", nil)
+	http.HandleFunc("/ping", home)
+	http.ListenAndServe(":8080", nil)
 
-	app := iris.New()
-
-	app.Get("/", func(ctx context.Context) {
-		log.Infof("no ping")
-		ctx.WriteString("ping pong")
-	})
-
-	app.Get("/ping", func(ctx context.Context) {
-		log.Infof("ping")
-		ctx.WriteString("pong pong")
-	})
-	app.Run(iris.Addr(":8080"), iris.WithCharset("UTF-8"))
+	//app := iris.New()
+	//
+	//app.Get("/", func(ctx context.Context) {
+	//	log.Infof("no ping")
+	//	ctx.WriteString("ping pong")
+	//})
+	//
+	//app.Get("/ping", func(ctx context.Context) {
+	//	log.Infof("ping")
+	//	ctx.WriteString("pong pong")
+	//})
+	//app.Run(iris.Addr(":8080"), iris.WithCharset("UTF-8"))
 
 	fmt.Println("Hello ks docker 2")
 
 }
 
-//func home(w http.ResponseWriter, r *http.Request) {
-//	fmt.Println("welcome to ks travel")
-//	fmt.Fprintf(w, "welcome to ks travel")
-//}
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("welcome to ks travel")
+	fmt.Fprintf(w, "welcome to ks travel")
+}
